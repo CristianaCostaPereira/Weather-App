f34a8a3cab6144d1626346a91ef9cafd0fa7dca6
CrisitianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-09-04 22:52:15 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+// Empty JS object to act as endpoint for all routes including as the API endpoint
+projectData = {};
+
+// Express package to run the server and its routes
+const express = require ("express");
+
+// Initiate an instance of app
+const app = express();
+
+// Dependencies - Middleware
+const bodyParser = require ("body-parser");
+
+// Middleware - connect to the app instance
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cross origin allowance
+const cors = require("cors");
+app.use(cors());
+
+// Direct my app to connect the server-side code to my main project folder AKA my client-side code
+app.use(express.static("website"));
+
+// Creat local server
+const port = 8000;
+
+const server = app.listen (port, () => {
+    console.log("server is running");
+    console.log(`running on localhost:${port}`);
+});
+
+// Respond with JS object when a GET route request is made to the homepage
+// "/all" so that the route triggers the GET request and returns the JS object
+app.get("/all", sendData);
+
+function sendData (req, res) {
+    res.send(projectData);
+};
+
+// POST Route:
+
+// Empty array to hold data
+const data = [];
+
+// First argument created is the URL I want to use and creat an API to add infos about the weather
+app.post("/addWeather", addWeather);
+
+// Obtains info from request.body and push into the array to have access to it from anywhere within the app
+function addWeather (req, res) {
+    data.push(req.body);
+    console.log(data);
+}
+
+// Process the data received from my POST Request
+// Set a variable named data to hold the value of request.body and print data to see what we receive
+app.post('/addWeather', function (req, res) {
+    let data = req.body;
+    console.log(data);
+});
---
+++ website/app.js
@@ -0,0 +1,23 @@
+// Post request client-side code
+const postData = async (url = "/addWeather", data = {}) => {
+    console.log(data);
+    const response = await fetch(url, {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "aplication.json",
+        },
+        body: JSON.stringify(data), // the body allows the access to the data on the server side
+    });
+
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData
+    } catch(error) {
+        console.log("error", error);
+    }
+}
+
+// Call function and pass in the path created
+postData("/addWeather", {weather:"sunny", temperature: 34});
