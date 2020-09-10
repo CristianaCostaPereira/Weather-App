7d63f56958fe9cd3053be1c0cdf008accd07cf20
CrisitianaCostaPereira <cristiana.vc.pereira@gmail.com>
2020-09-10 15:11:36 +0100

    Final commit

--- website/app.js
+++ website/app.js
@@ -101,9 +101,8 @@ var feelingsInput = document.getElementById("feelings");
 feelingsInput.addEventListener("keyup", function(event) {
 
   if (event.keyCode === 13) {
-
     event.preventDefault();
 
-    performAction(); 
+    performAction();
   }
 });
